97007fe32be661066a3c1a6e8c15b9b82fcd6421
Matt Blickem <explosivose@gmail.com>
2018-07-04 23:36:05 +0100

    Add assets

--- Assets/Scripts/Minnow.cs
+++ Assets/Scripts/Minnow.cs
@@ -7,6 +7,7 @@ public class Minnow : MonoBehaviour {
   private Boid _boid;
   private Rigidbody2D _rb;
   private Collider2D _collider;
+  private Transform _renderer;
 
   void Awake() {
     _rb = GetComponent<Rigidbody2D>();
@@ -19,6 +20,17 @@ public class Minnow : MonoBehaviour {
     if (!_boid) {
       Debug.LogError("Minnow Boid child requires Boid component");
     }
+    _renderer = transform.Find("Sprite");
+    if (!_renderer) {
+      Debug.LogError("Minnow requires Sprite child");
+    }
+  }
+
+  void Update() {
+    Vector3 velocity = _boid.boidDir;
+    // float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+    Quaternion rotation = Quaternion.LookRotation(Vector3.forward, velocity);
+    _renderer.rotation = Quaternion.Lerp(_renderer.rotation, rotation, 0.1f);
   }
 
   void StartEaten() {
